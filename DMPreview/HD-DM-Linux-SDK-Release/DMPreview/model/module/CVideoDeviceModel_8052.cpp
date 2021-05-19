1e46f6ba3d3116f2fa3378a772bcbb70898c4d7f
Alan Lin <alan.lin@eys3d.com>
2021-05-19 14:29:27 +0800

    Remove DMPreview Linux

--- DMPreview/HD-DM-Linux-SDK-Release/DMPreview/model/module/CVideoDeviceModel_8052.cpp
+++
@@ -1,7 +0,0 @@
-#include "CVideoDeviceModel_8052.h"
-
-CVideoDeviceModel_8052::CVideoDeviceModel_8052(DEVSELINFO *pDeviceSelfInfo):
-CVideoDeviceModel_8036_8052(pDeviceSelfInfo)
-{
-
-}
--- DMPreview/HD-DM-Linux-SDK-Release/console_tester/readme.txt
+++
@@ -1,7 +0,0 @@
-How to build console tool (For x86 64):
-
-sh build.sh
-
-How to run console tool:
-
-sh run_x86.sh
