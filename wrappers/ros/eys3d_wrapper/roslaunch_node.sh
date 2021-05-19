49bab14a6efea4ff889c1d48da96a072bd943e40
Alan Lin <alan.lin@eys3d.com>
2021-05-19 15:24:45 +0800

    remove ros

--- wrappers/ros/eys3d_wrapper/roslaunch_node.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-. ../eys3d_ros/eys3d_ros_ws/devel/setup.bash
-roslaunch dm_preview display.launch
--- wrappers/ros/eys3d_wrapper/src/eys3d_depth/device/RegisterSettings.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "eSPDI.h"
-
-class RegisterSettings {
-	RegisterSettings();
-public:
-    static int DM_Quality_Register_Setting(void* hEtronDI, PDEVSELINFO pDevSelInfo);
-};
